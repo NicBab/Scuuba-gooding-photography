ab6f594d63a4de96025ed13c68ed1949ce1d23b7
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-07-25 00:33:18 -0500

    navItem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,18 @@ import './css/Header.css'
 const Header = () => {
     return (
         <div className="header">
-         <div className="name">SG Photography</div>
-          <Nav id="nav">
-            <Nav.Item>
-              <Nav.Link><Link to="/">Home</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link><Link to="/shop">Shop</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
+          <div className="name">SG Photography</div>
+           <Nav id="nav">
+             <Nav.Item>
+               <Nav.Link><Link to="/">Home</Link></Nav.Link>
+              </Nav.Item>
+             <Nav.Item>
+               <Nav.Link><Link to="/shop">Shop</Link></Nav.Link>
+              </Nav.Item>
+             <Nav.Item>
+               <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
+              </Nav.Item>
+             <Nav.Item>
               <Nav.Link><Link to="/bio">Bio</Link></Nav.Link>
             </Nav.Item>
           </Nav>
