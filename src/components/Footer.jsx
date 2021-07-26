6f25d861dce6b66b1b3eb02552951811e4d91fca
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-07-26 16:59:23 -0500

    added face/insta Icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { instagram, facebook } from '../icons/index'
 import './css/Footer.css'
+
+const onInstaClick = () => {
+    window.open("")
+}
+
+const onFacebookClick = () => {
+    window.open("")
+}
 
 const Footer = () => {
     return (
         <div className="foot">
-            SG Photography@2021
-        </div>
+            <div className="footName">SG Photography@2021</div>
+            <div className="instagramIcon" onClick={onInstaClick}>{instagram}</div>
+            <div className="facebookIcon" onClick={onFacebookClick}>{facebook}</div>
+       </div>
     )
 }
 
