be1101f5389f121b8462ed40d57dd9b8e3894980
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-07-26 15:43:36 -0500

    navLink-css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,19 @@ import './css/Header.css'
 const Header = () => {
     return (
         <div className="header">
-          <div className="name">SG Photography</div>
+          <div className="name"><Link to="/" className="fa-title">SG Photography</Link></div>
            <Nav id="nav">
              <Nav.Item>
-               <Nav.Link><Link to="/">Home</Link></Nav.Link>
+               <Nav.Link><Link to="/" className="fa-home">Home</Link></Nav.Link>
               </Nav.Item>
              <Nav.Item>
-               <Nav.Link><Link to="/shop">Shop</Link></Nav.Link>
+               <Nav.Link><Link to="/shop" className="fa-shop">Shop</Link></Nav.Link>
               </Nav.Item>
              <Nav.Item>
-               <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
+               <Nav.Link><Link to="/contact"className="fa-contact">Contact</Link></Nav.Link>
               </Nav.Item>
              <Nav.Item>
-              <Nav.Link><Link to="/bio">Bio</Link></Nav.Link>
+              <Nav.Link><Link to="/bio" className="fa-bio">Bio</Link></Nav.Link>
             </Nav.Item>
           </Nav>
         </div>
