9ead332fdd56cdb99f95780aac7c008393ad6c51
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-08-10 15:50:26 -0500

    dummy gallery images

--- src/components/ImgCarousel.jsx
+++ src/components/ImgCarousel.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import ImageGallery from 'react-image-gallery'
 import './css/ImgCarousel.css'
 
 const ImgCarousel = () => {
- return null
+    const URL = "https://picsum.photos"
+    // const [images, setImages] = useState([])
+
+    // useEffect(() => {
+    //     axios.get(URL).then((res) => {
+    //         setImages(res.data.images)
+    //     })
+    // })
+    
+
+    let images = [
+        {
+            original: `${URL}/id/1018/1000/600/`,
+            thumbnail: `${URL}/id/1018/250/150/`,
+        },
+        {
+            original: `${URL}/id/1015/1000/600/`,
+            thumbnail: `${URL}/id/1015/250/150/`,
+        },
+        {
+            original: `${URL}/id/1019/1000/600/`,
+            thumbnail: `${URL}/id/1019/250/150/`,
+        },
+    ]; 
+
+    return <ImageGallery items={images} />
 }
 
 export default ImgCarousel
---
+++ src/components/LoadSpinner.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const LoadSpinner = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LoadSpinner
