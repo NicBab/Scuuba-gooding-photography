08f426a84d1c72e641ad1ad497826a617504128c
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-08-15 11:15:44 -0500

    clean home pg comp.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import './css/Home.css'
-import { Container } from 'react-bootstrap'
 import mesaSunBurst from '../images/MesaSunBurst.jpg'
 
 const Home = () => {
     return (
         <>
-          {/* <Container 
-            className="d-flex justify-content-center"
-            style={{maxWidth: "100vw", height: "auto"}}> */}
-            <div className="homeImg">
-                <img style={{maxWidth: "100%", height: "auto"}}
-                     src={mesaSunBurst}
-                     alt="Mesa SunBurst">
-                </img>
-             </div>
-          {/* </Container> */}
+          <div className="homeImg">
+              <img style={{maxWidth: "100%", height: "auto"}}
+                   src={mesaSunBurst}
+                   alt="Mesa SunBurst">
+              </img>
+          </div>
        </>
     )
 }
